e176eda90dd1360f4ea7fe1e8f92c9f2be4b0459
cevdetabbas <50247064+cevdetabbas@users.noreply.github.com>
2020-04-16 13:41:41 -0500

    Create uniquecharactersinanarray.java

---
+++ uniquecharactersinanarray.java
@@ -0,0 +1,55 @@
+public static String removeduplicate(String str)
+    {
+        String[] strr = str.split("");
+        int a = strr.length;
+        String result = "";
+        for (int i = 0 ; i<a ; i++)
+        {
+            int count = 0;
+            for (int j = 0 ; j<a ; j++)
+            {
+                if(j>i&& strr[i].equals(strr[j]))
+                {
+                    strr[j]="";
+                }
+            }
+        }
+        //System.out.println(Arrays.toString(strr));
+        for (int i = 0 ; i<a ; i++)
+        {
+            result = result + strr[i];
+        }
+        //System.out.println(result);
+        return result;
+    }
+
+    public static String unique(String str)
+    {
+        String[] strr = str.split("");
+        int a = strr.length;
+        String result = "";
+        for (int i = 0 ; i<a ; i++)
+        {
+            int count = 0;
+            for (int j = 0 ; j<a ; j++)
+            {
+                if(j>i&&strr[i].equals(strr[j]))
+                {
+                    strr[j]="";
+                    count += 1;
+                }
+            }
+            if (count==0)
+            {
+                result += strr[i];
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(removeduplicate("kamilmahumuhuseyin"));
+        System.out.println(unique("qwrrttqtttttttsssssttttttttt"));
+    }
