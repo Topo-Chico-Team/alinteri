b5cdfa8cd106d640be06f1f61d1668db7dce77d4
cevdetabbas <50247064+cevdetabbas@users.noreply.github.com>
2020-04-23 09:30:45 -0500

    Create printUniqueNumbers.java

---
+++ printUniqueNumbers.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main 
+{
+  public static void main(String[] args) 
+  {
+    Scanner scan = new Scanner(System.in);
+    int size = scan.nextInt();
+    int[] nums = new int[size];
+    for(int i=0; i < size; i++){
+      nums[i] = scan.nextInt();
+    }
+    printUniqueNumbers(nums);
+  }
+  
+  public static void printUniqueNumbers(int[] nums)
+  {
+    //WRITE YOUR CODE HERE
+			//int[] arr1 = new int[nums.length]
+			for (int i = 0 ; i<nums.length ; i++)
+			{
+				for (int j = 0 ; j<nums.length ; j++)
+				{
+					if(j>i&&nums[i]==nums[j])
+					{
+						nums[j]=0;
+						nums[i]=0;
+						continue;
+					}
+				}
+			}
+			for (int i = 0 ; i<nums.length ; i++)
+			{
+				if(nums[i]!=0)
+				{
+					System.out.println(nums[i]);
+				}
+			}
+  }
+}
+
+
+ 
