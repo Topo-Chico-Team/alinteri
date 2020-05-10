437bf15e55876e1c3ed322b218fca2f63fb1b7ab
cevdetabbas <cevdetabbas@gmail.com>
2020-05-10 01:28:17 -0500

    a

--- fibonacci.java
+++ fibonacci.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+public class fibonacci
+
 public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     int num = scan.nextInt();
@@ -5,12 +9,14 @@ public static void main(String[] args) {
   }
   
   public static void fib(int num){
-    //WRITE YOUR CODE HERE
-    
-		int[] arr = new int[num+1];
+		//WRITE YOUR CODE HERE
+
+		int[]arr=new int[num+1];
 		arr[0]=0;arr[1]=1;
-		for(int i =2 ; i<num+1 ; i++)
+		for(int i=2;i<num+1;i++)
 		{
-			arr[i]=arr[i-2]+arr[i-1];
+		arr[i]=arr[i-2]+arr[i-1];
 		}
 		System.out.println(arr[num]);
+		}
+
